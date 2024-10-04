af1eb42d4a383eeb9f1a5e33d0da049a462e0d86
shuvamshr <shuvamshrestha408@gmail.com>
2024-10-04 10:55:23 +1000

    Initial Commit

---
+++ AppleFoundationResources/AppleFoundationResourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFoundationResourcesApp.swift
+//  AppleFoundationResources
+//
+//  Created by HSIN on 4/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFoundationResourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
